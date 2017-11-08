1f428ea6c7a9b188c578f6913387d964673ee0d8
VitorLorente <lorente.sociologia@gmail.com>
2017-11-08 16:05:28 -0200

    app matricula com classe aluno e com as migrações feitas

--- matricula/models.py
+++ matricula/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Aluno(models.Model):
+    ra = models.IntegerField(unique=True)
+    nome = models.CharField(max_length=120)
+    email = models.CharField(max_length=80)
+    celular = models.CharField(max_length=11, blank=True, null=True)
+    sigla_curso = models.CharField(max_length=2)
+
+    class Meta:
+        db_table = 'ALUNO'
